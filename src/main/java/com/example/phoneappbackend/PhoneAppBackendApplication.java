bc6d3b0d7fb12f54e52eb82edbb1a869199d868c
r21219 <r21219@student.osu.cz>
2023-10-15 10:24:45 +0200

    Initial commit

---
+++ src/main/java/com/example/phoneappbackend/PhoneAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.phoneappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phoneappbackend/PhoneAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phoneappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
